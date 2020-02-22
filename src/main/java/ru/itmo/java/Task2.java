be4680cf235e9d0e97ea38c7b6b026d944cfc3e7
264269 <999all@mail.ru>
2020-02-22 14:05:13 +0300

    task2 re-done

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -10,22 +10,25 @@ public class Task2 {
      * - дано 17.9
      * - вывод 0.9
      */
-    double getFractionalPart(double realNumber) {
-        return (realNumber % 1); //your code here
+    double getFractionalPart(double realNumber)
+    {
+        return (realNumber % 1);
     }
 
     /**
      * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
      */
-    int charCode(char c) {
-        return c; //your code here
+    int charCode(char c)
+    {
+        return c;
     }
 
     /**
      * Даны 2 целых числа. Проверьте равны ли они
      */
-    boolean testIfIntsEqual(Integer a, Integer b) {
-        return (a-b==0); //your code here
+    boolean testIfIntsEqual(Integer a, Integer b)
+    {
+        return (a-b==0);
     }
 
     /**
@@ -39,7 +42,8 @@ public class Task2 {
      */
     boolean numberInRange(Integer number, Integer leftBound, Integer rightBound, Boolean inclusively)
     {
-        if (number == null || leftBound == null || rightBound == null || inclusively == null) {
+        if (number == null || leftBound == null || rightBound == null || inclusively == null)
+        {
             return false;
         }
         if (inclusively.booleanValue())
@@ -52,8 +56,10 @@ public class Task2 {
     /**
      * Даны 3 символа. Определите является ли хотя бы один из них цифрой 1..9
      */
-    boolean atLeastOneIsDigit(char c1, char c2, char c3) {
-        if (((c1 > 47) && (c1 < 58)) || ((c2 > 47) && (c2 < 58)) || ((c3 > 47) && (c3 < 58))) {
+    boolean atLeastOneIsDigit(char c1, char c2, char c3)
+    {
+        if (((c1 > 47) && (c1 < 58)) || ((c2 > 47) && (c2 < 58)) || ((c3 > 47) && (c3 < 58)))
+        {
            return true;
         }
         return false;
@@ -62,22 +68,25 @@ public class Task2 {
     /**
      * Даны два вещественных числа. Определите равны ли они
      */
-    boolean areRealNumbersEqual(double a, double b) {
+    boolean areRealNumbersEqual(double a, double b)
+    {
         return (a-b < 0.0000000000001); //your code here
     }
 
     /**
      * Дано трехзначное число 100 <= N <= 999. Найдите сумму его чисел.
      */
-    int sumOfDigits(int n) {
-        return (n/100 + n%100/10 + n%10); //your code here
+    int sumOfDigits(int n)
+    {
+        return (n/100 + n%100/10 + n%10);
     }
 
     /**
      * Дано натуральное число N <= 10000. Выведите следующее после N четное число. Например: N = 8, Result = 10
      */
-    int nextEvenNumber(int n) {
-        return (n+2); //your code here
+    int nextEvenNumber(int n)
+    {
+        return (n+2-n%2); //your code here
     }
 
     /**
@@ -86,15 +95,16 @@ public class Task2 {
      * Выведите минимальное кол-во парт, которые нужно приобрести школе, при условии что каждый из классов сидит в
      * своем кабинете
      */
-    int schoolDesks(int num1, int num2, int num3) {
+    int schoolDesks(int num1, int num2, int num3)
+    {
         return (num1/2 + num2/2 + num3/2 + num1%2 + num2%2 + num3%2); //your code here
     }
 
     /**
      * Дано натуральное число N >= 10. Выведите результат применения XOR к последним 2-м цифрам числа N
      */
-    int xorDigits(int N) {
+    int xorDigits(int N)
+    {
         return ((N%10)^(N%100/10)); //your code here
     }
-
 }
